18a4d087bd2edf61180b4e07f23cdda9e9e19bcf
anitaRead <areadman91@gmail.com>
2021-09-08 12:24:56 +0100

    Add app.js file with dependencies

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function (req, res) {
+  res.send('Team 2 is the best!');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
