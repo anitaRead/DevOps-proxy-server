1364ab872621ad6373ce5801acb895c431ccac23
Alex <alexandrachorbe@MA324.local>
2021-09-10 12:43:58 +0100

    trigger deploy

--- dev.sh
+++ dev.sh
@@ -1,3 +1,3 @@
 rm -rf node_modules devops-proxy-server.zip
 zip -r devops-proxy-server.zip .
-aws s3 cp ./devops-proxy-server.zip s3://team2-nodea-pp/
+aws s3 cp ./devops-proxy-server.zip s3://team2-proxy/
