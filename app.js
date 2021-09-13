1bd722a9d75589a6e334a4ca57d759f3a511e825
Alex <alexandrachorbe@MA324.local>
2021-09-13 15:09:30 +0100

    file app.js

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const app = express();
 // const https = require('https');
 const http = require('http');
+const request = require('request')
 
 const url = 'http://team-2-reliability-server.mkrs.link';
 
 
 app.get('/', function (req, res) {
-  http.get(url, function(res) { 
+  http.get(url)
+  .on('response', function(response) {
+    console.log(response.statusCode)
+    console.log(response)
+  })
+
+
   
-    if(res.statusCode !== 200) {
-      console.log('Inside conditional');
-      http.get(url);
-    } else {
-      console.log('Status code: ', res.statusCode);
-    }
-  
-  }).on('error', function(e) {
+  .on('error', function(e) {
     console.error(e);
   });
   res.send('Team 2 is the best!');
