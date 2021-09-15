0f96b7dbc67e525953163da3af5d2fd3f62b65e3
anitaRead <areadman91@gmail.com>
2021-09-15 12:18:39 +0100

    -

--- app.js
+++ app.js
@@ -3,32 +3,32 @@ const app = express();
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const TARGET_SERVER = "team-2-reliability-server.mkrs.link"
 
-app.get('/*', async (req, res) => {
-  let request = req.originalUrl
-  console.log(`:: GET ${request}`)
+// app.get('/*', async (req, res) => {
+//   let request = req.originalUrl
+//   console.log(`:: GET ${request}`)
 
-  let attemptsLeft = 3;
-  let upstreamResponse;
+//   let attemptsLeft = 3;
+//   let upstreamResponse;
 
-  while (attemptsLeft > 0) {
-    let upstream = `http://${TARGET_SERVER}${request}`;
-    console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
-    attemptsLeft = attemptsLeft - 1
-    upstreamResponse = await fetch(upstream, {
-      headers: { 'Authorization': req.header('Authorization') }
-    })
-    if (upstreamResponse.ok) {
-      let text = await upstreamResponse.text()
-      res.header('Content-Type', upstreamResponse.headers.get('content-type'))
-         .status(upstreamResponse.status)
-         .send(text)
-      console.log(":: Successful GET!")
-      return
-    }
-  }
-  console.log(`:: Failed GET ${request}`)
-  res.status(upstreamResponse.status).send(await upstreamResponse.text())
-})
+//   while (attemptsLeft > 0) {
+//     let upstream = `http://${TARGET_SERVER}${request}`;
+//     console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
+//     attemptsLeft = attemptsLeft - 1
+//     upstreamResponse = await fetch(upstream, {
+//       headers: { 'Authorization': req.header('Authorization') }
+//     })
+//     if (upstreamResponse.ok) {
+//       let text = await upstreamResponse.text()
+//       res.header('Content-Type', upstreamResponse.headers.get('content-type'))
+//          .status(upstreamResponse.status)
+//          .send(text)
+//       console.log(":: Successful GET!")
+//       return
+//     }
+//   }
+//   console.log(`:: Failed GET ${request}`)
+//   res.status(upstreamResponse.status).send(await upstreamResponse.text())
+// })
 
 app.post('/*', async (req, res) => {
   let request = req.originalUrl
