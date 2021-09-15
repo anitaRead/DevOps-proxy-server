5e6b1dfde6d3f96eed3bce81f4e5f389ac4d1edf
anitaRead <areadman91@gmail.com>
2021-09-15 10:53:05 +0100

    Update fetch post request to include method and body

--- app.js
+++ app.js
@@ -33,7 +33,9 @@ app.get('/*', async (req, res) => {
 app.post('/*', async (req, res) => {
   let request = req.originalUrl
   console.log(`:: POST ${request}`)
-
+  console.log('********************************************************************************');
+  console.log(req.body);
+  console.log('*********************************************************************************');
   let attemptsLeft = 3;
   let upstreamResponse;
 
@@ -42,6 +44,8 @@ app.post('/*', async (req, res) => {
     console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
     attemptsLeft = attemptsLeft - 1
     upstreamResponse = await fetch(upstream, {
+      method: 'post',
+	    body: JSON.stringify(req.body),
       headers: { 'Authorization': req.header('Authorization') }
     })
     if (upstreamResponse.ok) {
@@ -57,59 +61,59 @@ app.post('/*', async (req, res) => {
   res.status(upstreamResponse.status).send(await upstreamResponse.text())
 })
 
-app.patch('/*', async (req, res) => {
-  let request = req.originalUrl
-  console.log(`:: PATCH ${request}`)
+// app.patch('/*', async (req, res) => {
+//   let request = req.originalUrl
+//   console.log(`:: PATCH ${request}`)
 
-  let attemptsLeft = 3;
-  let upstreamResponse;
+//   let attemptsLeft = 3;
+//   let upstreamResponse;
 
-  while (attemptsLeft > 0) {
-    let upstream = `http://${TARGET_SERVER}${request}`;
-    console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
-    attemptsLeft = attemptsLeft - 1
-    upstreamResponse = await fetch(upstream, {
-      headers: { 'Authorization': req.header('Authorization') }
-    })
-    if (upstreamResponse.ok) {
-      let text = await upstreamResponse.text()
-      res.header('Content-Type', upstreamResponse.headers.get('content-type'))
-         .status(upstreamResponse.status)
-         .send(text)
-      console.log(":: Successful PATCH!")
-      return
-    }
-  }
-  console.log(`:: Failed PATCH ${request}`)
-  res.status(upstreamResponse.status).send(await upstreamResponse.text())
-})
+//   while (attemptsLeft > 0) {
+//     let upstream = `http://${TARGET_SERVER}${request}`;
+//     console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
+//     attemptsLeft = attemptsLeft - 1
+//     upstreamResponse = await fetch(upstream, {
+//       headers: { 'Authorization': req.header('Authorization') }
+//     })
+//     if (upstreamResponse.ok) {
+//       let text = await upstreamResponse.text()
+//       res.header('Content-Type', upstreamResponse.headers.get('content-type'))
+//          .status(upstreamResponse.status)
+//          .send(text)
+//       console.log(":: Successful PATCH!")
+//       return
+//     }
+//   }
+//   console.log(`:: Failed PATCH ${request}`)
+//   res.status(upstreamResponse.status).send(await upstreamResponse.text())
+// })
 
-app.delete('/*', async (req, res) => {
-  let request = req.originalUrl
-  console.log(`:: DELETE ${request}`)
+// app.delete('/*', async (req, res) => {
+//   let request = req.originalUrl
+//   console.log(`:: DELETE ${request}`)
 
-  let attemptsLeft = 3;
-  let upstreamResponse;
+//   let attemptsLeft = 3;
+//   let upstreamResponse;
 
-  while (attemptsLeft > 0) {
-    let upstream = `http://${TARGET_SERVER}${request}`;
-    console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
-    attemptsLeft = attemptsLeft - 1
-    upstreamResponse = await fetch(upstream, {
-      headers: { 'Authorization': req.header('Authorization') }
-    })
-    if (upstreamResponse.ok) {
-      let text = await upstreamResponse.text()
-      res.header('Content-Type', upstreamResponse.headers.get('content-type'))
-         .status(upstreamResponse.status)
-         .send(text)
-      console.log(":: Successful DELETE!")
-      return
-    }
-  }
-  console.log(`:: Failed DELETE ${request}`)
-  res.status(upstreamResponse.status).send(await upstreamResponse.text())
-})
+//   while (attemptsLeft > 0) {
+//     let upstream = `http://${TARGET_SERVER}${request}`;
+//     console.log(`:: Attempt ${4 - attemptsLeft}: ${upstream}`)
+//     attemptsLeft = attemptsLeft - 1
+//     upstreamResponse = await fetch(upstream, {
+//       headers: { 'Authorization': req.header('Authorization') }
+//     })
+//     if (upstreamResponse.ok) {
+//       let text = await upstreamResponse.text()
+//       res.header('Content-Type', upstreamResponse.headers.get('content-type'))
+//          .status(upstreamResponse.status)
+//          .send(text)
+//       console.log(":: Successful DELETE!")
+//       return
+//     }
+//   }
+//   console.log(`:: Failed DELETE ${request}`)
+//   res.status(upstreamResponse.status).send(await upstreamResponse.text())
+// })
 
 
 app.listen(process.env.PORT || '80', function () {
